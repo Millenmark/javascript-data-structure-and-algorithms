9e679b8da4eb95fffa2a8eb8c59aaf0886dae143
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-30 21:11:54 +0800

    Update summation.js

--- Summation/summation.js
+++ Summation/summation.js
@@ -18,3 +18,19 @@ console.log(summation2(7));
 function oddEven(n) {
   return n % 2 === 0 ? 'even' : 'odd'
 }
+
+function sigmaNotation(n, times){
+  let sum = 0
+  for (let i = 1; i <= times; i++){
+    sum += n * (i)
+  }
+  return sum
+} //This is O(n)
+
+function sigmaNotation2(n, times) {
+  const sum = (times * (times + 1)) / 2
+  return n * sum
+}//This is O(1)
+
+console.log(sigmaNotation(4, 7));
+console.log(sigmaNotation2(4, 6));
