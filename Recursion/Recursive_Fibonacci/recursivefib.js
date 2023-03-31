154763b4dff0cb4d87bb8a4bd6ff4e2672640421
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-31 13:19:01 +0800

    update

--- Recursion/Recursive_Fibonacci/recursivefib.js
+++ Recursion/Recursive_Fibonacci/recursivefib.js
@@ -2,7 +2,7 @@ function recursiveFibonacci(n) {
   if (n < 2) return n
   return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
 }
+// console.log(recursiveFibonacci(1000)) //This will take time
+//O(n) = iterative
+//O(2^n) = recursive
 
-console.log(recursiveFibonacci(0))
-console.log(recursiveFibonacci(1))
-console.log(recursiveFibonacci(6))
