ebc8f85e0d2b5430789c40f79e6dda2bc18e3393
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-01 18:34:19 +0800

    Update reverse.js

--- SortingAlgorithm/reverse.js
+++ SortingAlgorithm/reverse.js
@@ -14,5 +14,19 @@ function reverseSort(arr) {
 
 var arr = [5, 2, 8, 3, 1, 4];
 var sortedArr = reverseSort(arr);
-console.log(sortedArr);
+// console.log(sortedArr);
 
+//Reversing a sorted array
+
+function reverseSorted(arr){
+  let length = arr.length
+  let middle = Math.floor(length / 2)
+  for ( let i = 0; i <= middle; i++){
+    let temp = arr[i]
+    arr[i] = arr[length - i - 1]
+    arr[length - i - 1] = temp
+  }
+  return arr
+}
+
+console.log(reverseSorted([50, 60, 70, 80, 90]));
