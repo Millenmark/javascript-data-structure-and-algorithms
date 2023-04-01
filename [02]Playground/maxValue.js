f6bc78030135fc68ea7501b22381b8769819cecd
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-01 12:23:48 +0800

    Update maxValue.js

---
+++ [02]Playground/maxValue.js
@@ -0,0 +1,13 @@
+function maxValue(arr) {
+  for (let i = 0; i < arr.length - 1; i++){
+    if (arr[i] > arr[i + 1]) {
+      let temp = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+    }
+  }
+  let max = arr[arr.length - 1]
+  return `The max value is: ${max}`
+}//I use bubble sort in this 
+
+console.log(maxValue([60, 50, 95, 80, 70]))
