514c3efe2bb1a3a514a68fa47286a71753de70a2
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-22 20:24:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+class Cookie {
+  constructor(color) {
+    this._color = color;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  sayHello() {
+    return `Hello Cookie ${this.color}`;
+  }
+}
+
+let cookieOne = new Cookie("green");
+
+cookieOne.color = "red";
+
+console.log(cookieOne.sayHello());
